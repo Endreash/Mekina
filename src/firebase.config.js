210a41f4723054887ef7e86c4340741b7acc4f55
Endreas Haileselassie <endreash@Endreass-MacBook-Pro.local>
2023-10-11 10:20:36 +0300

    updated config

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,12 +6,12 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBszeiAHFecjGdSv9eo1uiRtBLRZMWB8aE",
-  authDomain: "bettys-place.firebaseapp.com",
-  projectId: "bettys-place",
-  storageBucket: "bettys-place.appspot.com",
-  messagingSenderId: "746790025581",
-  appId: "1:746790025581:web:fab7b4d5b44e83572f4264",
+  apiKey: APIKEY,
+  authDomain: AUTHDOMAIN,
+  projectId: PROJECTID,
+  storageBucket:  STORAGEBUCKET,
+  messagingSenderId:MESSAGINGSENDERID,
+  appId: APPID,
 };
 
 // Initialize Firebase
